574dc07b243eb5a351d47802e37912791b8964d7
VictorTurek <victorturekbcn@gmail.com>
2024-01-24 11:45:07 +0100

    working on cards

--- budgetApp/src/app/components/product-options/product-options.component.ts
+++ budgetApp/src/app/components/product-options/product-options.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HomeComponent } from '../../pages/home/home.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-options',
   standalone: true,
-  imports: [],
+  imports: [HomeComponent, FormsModule, CommonModule],
   templateUrl: './product-options.component.html',
   styleUrl: './product-options.component.sass'
 })
 export class ProductOptionsComponent {
+  @Input() data: any;
+
+  showOptions: boolean = false;
+  pageNumber: number = 0;
+  languageNumber: number = 0;
+
+  toggleOptions() {
+    // Toggle the visibility of options when the checkbox is clicked
+    this.showOptions = !this.showOptions;
+  }
 
 }
--- budgetApp/src/app/pages/home/home.component.ts
+++ budgetApp/src/app/pages/home/home.component.ts
@@ -2,14 +2,20 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { ProductOptionsComponent } from '../../components/product-options/product-options.component';
 import { CurrentBudgetsComponent } from '../../components/current-budgets/current-budgets.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, ProductOptionsComponent, CurrentBudgetsComponent],
+  imports: [HeaderComponent, ProductOptionsComponent, CurrentBudgetsComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass'
 })
 export class HomeComponent {
+  objects = [
+    { title: 'SEO', description: 'Analisis SEO de una pagina web', price: '300€' },
+    { title: 'Ads', description: 'Creacio duna campanya de Publi', price: '400€' },
+    { title: 'Web', description: 'Programaco duna web responsive', price: '500€' },
+  ];
 
 }
